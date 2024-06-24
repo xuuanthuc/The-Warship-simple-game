4a5962bb9b2fd6ac5c8d06d4289adc24a7bcd3c4
dxthuc <dxthuc@cmcglobal.vn>
2024-06-24 17:29:02 +0700

    updatre scaffold

--- lib/src/screens/game_play/widget/game_over.dart
+++ lib/src/screens/game_play/widget/game_over.dart
@@ -7,40 +7,37 @@ class GameOverOverlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red,
-      body: BlocBuilder<GamePlayCubit, GamePlayState>(
-        builder: (context, state) {
-          final room = state.room;
-          final player = state.player;
-          final hostPlayer = room.ownerPlayer;
-          final iamHost = hostPlayer != null && player?.id == hostPlayer.id;
+    return BlocBuilder<GamePlayCubit, GamePlayState>(
+      builder: (context, state) {
+        final room = state.room;
+        final player = state.player;
+        final hostPlayer = room.ownerPlayer;
+        final iamHost = hostPlayer != null && player?.id == hostPlayer.id;
 
-          if (!state.room.guestPlayingData!.occupiedBlocks
-              .any((s) => s.overlappingPositions.isNotEmpty)) {
-            return Text(
-              iamHost ? "Game over: Owner win" : "You lost",
-              style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.w700,
-                color: Colors.blue,
-              ),
-            );
-          }
-          if (!state.room.ownerPlayingData!.occupiedBlocks
-              .any((s) => s.overlappingPositions.isNotEmpty)) {
-            return Text(
-              iamHost ? "You lost" : "Game over: Guest win",
-              style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.w700,
-                color: Colors.blue,
-              ),
-            );
-          }
-          return Container();
-        },
-      ),
+        if (!state.room.guestPlayingData!.occupiedBlocks
+            .any((s) => s.overlappingPositions.isNotEmpty)) {
+          return Text(
+            iamHost ? "Game over: Owner win" : "You lost",
+            style: TextStyle(
+              fontSize: 45,
+              fontWeight: FontWeight.w700,
+              color: Colors.blue,
+            ),
+          );
+        }
+        if (!state.room.ownerPlayingData!.occupiedBlocks
+            .any((s) => s.overlappingPositions.isNotEmpty)) {
+          return Text(
+            iamHost ? "You lost" : "Game over: Guest win",
+            style: TextStyle(
+              fontSize: 45,
+              fontWeight: FontWeight.w700,
+              color: Colors.blue,
+            ),
+          );
+        }
+        return Container();
+      },
     );
   }
 }
